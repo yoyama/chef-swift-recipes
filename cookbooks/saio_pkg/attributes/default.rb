0fd1996c8c9b27fa76ddab0f16450754fc93e1d3
You Yamagata <bi.yamagata@gmail.com>
2013-08-14 18:50:30 +0900

    Update packages version.

--- cookbooks/saio_pkg/attributes/default.rb
+++ cookbooks/saio_pkg/attributes/default.rb
@@ -7,11 +7,11 @@ default[:swift][:log_level]= "DEBUG"
 default[:swift][:proxy_host]= "127.0.0.1"
 default[:swift][:memcached_ips]= "127.0.0.1"
 default[:swift][:cloudarchive_repo] = "grizzly"
-default[:swift][:pkg_version] = "1.8.0-0ubuntu1~cloud0"
+default[:swift][:pkg_version] = "1.8.0-0ubuntu1.2~cloud0"
 default[:swift][:client_pkg_version] = "1:1.3.0-0ubuntu1~cloud0"
 
-default[:keystone][:pkg_version] = "1:2013.1-0ubuntu1~cloud0"
-default[:keystone][:client_pkg_version] = "1:0.2.3-0ubuntu1~cloud0"
+default[:keystone][:pkg_version] = "1:2013.1.2-0ubuntu2~cloud0"
+default[:keystone][:client_pkg_version] = "1:0.2.3-0ubuntu2.2~cloud0"
 default[:keystone][:ks_auth_host_internal] = 'localhost'
 default[:keystone][:ks_auth_host_external] = 'localhost'
 default[:keystone][:ks_admin_token] = 'OpenStack'
