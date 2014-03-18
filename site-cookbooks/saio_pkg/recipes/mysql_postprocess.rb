3801c964704abc04746dfa8ad856b97f66bcb375
Ubuntu <ubuntu@ip-10-111-1-123.(none)>
2014-03-18 12:01:41 +0900

    Fix the issue that mysql not auto start at boot.

--- site-cookbooks/saio_pkg/recipes/mysql_postprocess.rb
+++ site-cookbooks/saio_pkg/recipes/mysql_postprocess.rb
@@ -2,6 +2,11 @@
 # Cookbook Name:: saio_pkg
 # Recipe:: mysql_postprocess
 #
+service "mysql" do
+  provider Chef::Provider::Service::Upstart
+  action [:enable, :start]
+end
+
 execute "restart mysql" do
   command "service mysql restart"
   user "root"
